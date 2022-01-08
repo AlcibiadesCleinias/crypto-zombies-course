3dbc4a29cd45657eb8b2bbd496d9db06026c093f
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-01-08 19:07:02 +0300

    first success test

--- lesson-truffle/contracts/CryptoZombies.sol
+++ lesson-truffle/contracts/CryptoZombies.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.24;
 
 import "./zombieownership.sol";
 
+/// @title ZombieOwnership with Self-destraction
+/// @dev At least for testing purpose...
 contract CryptoZombies is ZombieOwnership {
 
-  /// @title Self-destraction
-  /// @dev At least for testing purpose...
   function kill() public onlyOwner {
     selfdestruct(owner());
   }
