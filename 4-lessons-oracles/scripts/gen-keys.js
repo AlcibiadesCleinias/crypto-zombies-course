b2489be03e01371f9d44711b9b385d5d449a5646
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-01-09 16:20:02 +0300

    add gen keys script

---
+++ 4-lessons-oracles/scripts/gen-keys.js
@@ -0,0 +1,17 @@
+// To generate private keys:
+// node scripts/gen-key.js oracle/oracle_private_key
+// node scripts/gen-key.js caller/caller_private_key
+
+const { CryptoUtils } = require('loom-js')
+const fs = require('fs')
+
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " <filename>.")
+    process.exit(1);
+}
+
+const privateKey = CryptoUtils.generatePrivateKey()
+const privateKeyString = CryptoUtils.Uint8ArrayToB64(privateKey)
+
+let path = process.argv[2]
+fs.writeFileSync(path, privateKeyString)
